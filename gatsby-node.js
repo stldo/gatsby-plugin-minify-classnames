0886eabe6fcb847a3ee212b0a110fd11e52f0ec9
stldo <st.shared@icloud.com>
2021-12-13 20:56:44 -0300

    refactor: improved CSS Modules detection

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,10 @@
-const incstr = require('incstr')
+const getMinifiedIdent = require('./lib/get-minified-ident')
 
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 const LETTER = /[A-Za-z]/
-
-const minifiedParts = new Map()
-
-function minifyIdent (identParts, dictionary) {
-  let ident = ''
-  let isFirst = true
-
-  for (const identPart in identParts) {
-    let minifiedIdents = minifiedParts.get(identPart)
-
-    if (!minifiedIdents) {
-      minifiedIdents = new Map()
-      minifiedIdents.nextId = incstr.idGenerator({ alphabet: dictionary })
-      minifiedParts.set(identPart, minifiedIdents)
-    }
-
-    const sourceIdent = identParts[identPart]
-
-    let minifiedIdent = minifiedIdents.get(sourceIdent)
-
-    if (!minifiedIdent) {
-      do {
-        minifiedIdent = minifiedIdents.nextId()
-        // eslint-disable-next-line no-unmodified-loop-condition
-      } while (isFirst && !LETTER.test(minifiedIdent[0]))
-
-      minifiedIdents.set(sourceIdent, minifiedIdent)
-    }
-
-    ident += `_${minifiedIdent}`
-    isFirst = false
-  }
-
-  return ident.slice(1) // Ignore the first underscore
-}
 
 exports.onCreateWebpackConfig = ({ actions, getConfig }, {
   dictionary = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ0123456789',
-  enable = IS_PRODUCTION,
+  enable = process.env.NODE_ENV === 'production',
   prefix = '',
   suffix = ''
 }) => {
@@ -53,22 +17,17 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }, {
   const config = getConfig()
 
   for (const { oneOf } of config.module.rules) {
-    if (!oneOf || !oneOf.length) {
+    if (!oneOf?.length) {
       continue
     }
 
-    for (const { use, test } of oneOf) {
-      if (
-        !test.test('.module.css') &&
-        !test.test('.module.less') &&
-        !test.test('.module.sass') &&
-        !test.test('.module.styl')
-      ) {
+    for (const { use } of oneOf) {
+      if (!use) {
         continue
       }
 
-      for (const { loader, options = {} } of use) {
-        if (!loader.includes('/css-loader/') || !options.modules) {
+      for (const { loader, options } of use) {
+        if (!loader?.includes('/css-loader/') || !options?.modules) {
           continue
         }
 
@@ -76,8 +35,7 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }, {
 
         modules.getLocalIdent = (context, _, localName) => {
           const identParts = { path: context.resourcePath, localName }
-          const minifiedIdent = minifyIdent(identParts, dictionary)
-
+          const minifiedIdent = getMinifiedIdent(identParts, dictionary)
           return `${prefix}${minifiedIdent}${suffix}`
         }
 
